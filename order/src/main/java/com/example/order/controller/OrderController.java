88037630e7128e1098b420b9983baa309e00692c
efa <efatariku07@gmail.com>
2026-08-08 15:05:31 +0300

    modified:   order/src/main/java/com/example/order/controller/OrderController.java 	new file:   order/src/main/java/com/example/order/service/InventoryClient.java 	modified:   order/src/main/resources/application.properties

--- order/src/main/java/com/example/order/controller/OrderController.java
+++ order/src/main/java/com/example/order/controller/OrderController.java
@@ -1,16 +1,28 @@
 package com.example.order.controller;
 
+import com.example.order.service.InventoryClient;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/orders")
 public class OrderController {
 
+    private final InventoryClient inventoryClient;
+
+    public OrderController(
+            InventoryClient inventoryClient
+    ) {
+        this.inventoryClient = inventoryClient;
+    }
+
     @PostMapping
     public String createOrder(
             @RequestParam Long productId
     ) {
 
-        return "Order created for product " + productId;
+        String inventoryResponse =
+                inventoryClient.reserveStock(productId);
+
+        return "Order created. " + inventoryResponse;
     }
 }
---
+++ order/src/main/java/com/example/order/service/InventoryClient.java
@@ -0,0 +1,24 @@
+package com.example.order.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
+
+@Service
+public class InventoryClient {
+    
+    private final RestClient restClient;
+
+    public InventoryClient() {
+        this.restClient = RestClient.builder()
+                .baseUrl("http://localhost:8082")
+                .build();
+    }
+
+    public String reserveStock(Long productId) {
+        return restClient
+                    .post()
+                    .uri("/api/inventory/{productId}/reserve", productId)
+                    .retrieve()
+                    .body(String.class);
+    }
+}
