1a9c877bbb6d1642729d3f380b51cfd55f43ed85
efa <efatariku07@gmail.com>
2026-08-14 08:52:08 +0300

    new file:   inventory/src/main/java/com/example/inventory/messaging/OrderEventConsumer.java

---
+++ inventory/src/main/java/com/example/inventory/messaging/OrderEventConsumer.java
@@ -0,0 +1,16 @@
+package com.example.inventory.messaging;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderEventConsumer {
+
+    @RabbitListener(queues = "order.queue")
+    public void consume(String message) {
+
+        System.out.println(
+                "Received order event: " + message
+        );
+    }
+}
